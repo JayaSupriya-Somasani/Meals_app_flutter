ecc80ee6e03e0e05c260b42e37915b721db9abaa
jayasupriyasomasani <jayasupriya.somasani@narayanagroup.com>
2023-03-31 11:43:28 +0530

    Category screen is implemented with grid view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './categories_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,31 +16,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Good meals'),
+      home: CategoriesScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Good meals"),
-      ),
-      body: const Center(
-        child: Text("Home page"),
-      ),
-    );
-  }
-}
